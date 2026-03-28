7b479f473dd7f6a8074c5fb003e3078313ad58a5
adamridene1107 <adam.ridene1107@gmail.com>
2026-03-28 15:19:31 +0100

    v1.0.0 - deploiement final

--- write_quiz.js
+++
@@ -1,4 +0,0 @@
-const fs=require('fs')
-const data=[{subject:'Maths',emoji:'x',color:'from-blue-500 to-indigo-600',themes:[]}]
-fs.writeFileSync('src/lib/quizData.js','export const QUIZ_DATA = '+JSON.stringify(data)+';')
-console.log('done')
